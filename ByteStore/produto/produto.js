504a6435ce070610260be6c703dc938af4bfae2c
Gabriel <gabrielferrarez77@gmail.com>
2025-05-22 21:23:01 -0300

    Criando Sistema com Vite e Criando Homepage

--- ByteStore/produto/produto.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.thumbnails img').forEach(thumb => {
-  thumb.addEventListener('click', function () {
-    const mainImg = document.querySelector('.image-section img');
-    const temp = mainImg.src;
-    mainImg.src = this.src;
-    this.src = temp;
-  });
-});
