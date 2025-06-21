e190ed1832298ddcb56ed37bdb8d415d2440b979
Gabriel <gabrielferrarez77@gmail.com>
2025-06-21 13:16:15 -0300

    Subindo o arquivo do banco de dados atualizado

--- database/bytestore.sql
+++ database/bytestore.sql
@@ -1,13 +1,13 @@
 -- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: bytestore
+-- Host: localhost    Database: bytestore
 -- ------------------------------------------------------
 -- Server version	8.0.42
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -18,7 +18,6 @@
 --
 -- Table structure for table `administradores`
 --
-
 CREATE DATABASE IF NOT EXISTS bytestore;
 USE bytestore;
 
@@ -31,9 +30,10 @@ CREATE TABLE `administradores` (
   `email` varchar(100) NOT NULL,
   `senha` varchar(255) NOT NULL,
   `funcao` varchar(50) DEFAULT NULL,
+  `token_reset` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +42,7 @@ CREATE TABLE `administradores` (
 
 LOCK TABLES `administradores` WRITE;
 /*!40000 ALTER TABLE `administradores` DISABLE KEYS */;
-INSERT INTO `administradores` VALUES (1,'Admin 1','adm1@admin.com','admin123','Gerente'),(2,'Admin 2','adm2@admin.com','admin456','Supervisor');
+INSERT INTO `administradores` VALUES (1,'Admin 1','adm1@admin.com','admin123','Gerente',NULL),(2,'Admin 2','adm2@admin.com','admin456','Supervisor',NULL),(7,'Gabriel Antônio','gabrielferrarez77@gmail.com','admin789','Gerente',NULL),(9,'Gabriel Antonio','gabrielferrarez777@gmail.com','admin789','Gerente',NULL);
 /*!40000 ALTER TABLE `administradores` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -76,6 +76,43 @@ LOCK TABLES `carrinho` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `usuario_id` int NOT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `data_nascimento` date DEFAULT NULL,
+  `cep` varchar(9) NOT NULL,
+  `estado` varchar(2) NOT NULL,
+  `cidade` varchar(100) NOT NULL,
+  `bairro` varchar(100) NOT NULL,
+  `rua` varchar(100) NOT NULL,
+  `numero` varchar(10) NOT NULL,
+  `complemento` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `cpf` (`cpf`),
+  KEY `fk_usuario` (`usuario_id`),
+  CONSTRAINT `clientes_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`),
+  CONSTRAINT `fk_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES (1,1,'123.456.789-00','1990-01-01','12345-678','SP','São Paulo','Centro','Rua Principal','100','Apto 101'),(2,14,'455.843.228-32','2025-06-19','14400-000','SP','Franca','Centro','Avenida Brasil','5050','AP11'),(3,15,'455.843.228-30','2025-06-07','14400-000','SP','Franca','Centro','Avenida Brasil','5050','AP11');
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `pedido_itens`
 --
 
@@ -93,7 +130,7 @@ CREATE TABLE `pedido_itens` (
   KEY `produto_id` (`produto_id`),
   CONSTRAINT `pedido_itens_ibfk_1` FOREIGN KEY (`pedido_id`) REFERENCES `pedidos` (`id`),
   CONSTRAINT `pedido_itens_ibfk_2` FOREIGN KEY (`produto_id`) REFERENCES `produtos` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -102,6 +139,7 @@ CREATE TABLE `pedido_itens` (
 
 LOCK TABLES `pedido_itens` WRITE;
 /*!40000 ALTER TABLE `pedido_itens` DISABLE KEYS */;
+INSERT INTO `pedido_itens` VALUES (3,2,1,1,3500.00),(4,2,2,2,80.00),(5,3,2,1,80.00),(6,4,5,1,350.00),(7,5,1,1,3500.00),(8,6,1,2,3500.00);
 /*!40000 ALTER TABLE `pedido_itens` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -119,10 +157,11 @@ CREATE TABLE `pedidos` (
   `status` varchar(50) DEFAULT NULL,
   `forma_pagamento` varchar(50) DEFAULT NULL,
   `endereco_entrega` varchar(255) DEFAULT NULL,
+  `valor_total` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `usuario_id` (`usuario_id`),
   CONSTRAINT `pedidos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -131,6 +170,7 @@ CREATE TABLE `pedidos` (
 
 LOCK TABLES `pedidos` WRITE;
 /*!40000 ALTER TABLE `pedidos` DISABLE KEYS */;
+INSERT INTO `pedidos` VALUES (2,14,'2025-06-20 10:31:22','Em processamento','Cartão de crédito','Rua Teste, 123',NULL),(3,14,'2025-06-20 10:50:50','pendente','cartão','Avenida Brasil, 5050 - Centro',80.00),(4,15,'2025-06-20 11:20:22','pendente','cartão','Avenida Brasil, 5050 - Centro',350.00),(5,15,'2025-06-20 14:27:42','pendente','cartão','Avenida Brasil, 5050 - Centro',3500.00),(6,14,'2025-06-20 16:51:09','pendente','cartão','Avenida Brasil, 5050 - Centro',7000.00);
 /*!40000 ALTER TABLE `pedidos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -148,8 +188,9 @@ CREATE TABLE `produtos` (
   `marca` varchar(100) DEFAULT NULL,
   `descricao` text,
   `preco` decimal(10,2) NOT NULL,
+  `destaque` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -158,7 +199,7 @@ CREATE TABLE `produtos` (
 
 LOCK TABLES `produtos` WRITE;
 /*!40000 ALTER TABLE `produtos` DISABLE KEYS */;
-INSERT INTO `produtos` VALUES (1,'Notebook Dell','notebook_dell.jpg','Dell','Notebook Dell i5, 8GB RAM, SSD 256GB',3500.00),(2,'Mouse Logitech','mouse_logitech.jpg','Logitech','Mouse sem fio Logitech M170',80.00),(3,'Teclado Mecânico Redragon','teclado_redragon.jpg','Redragon','Teclado mecânico Redragon Kumara',250.00);
+INSERT INTO `produtos` VALUES (1,'Notebook Dell','notebook_dell.jpg','Dell','Notebook Dell i5, 8GB RAM, SSD 256GB',3500.00,0),(2,'Mouse Logitech','mouse_logitech.jpg','Logitech','Mouse sem fio Logitech M170',80.00,0),(3,'Teclado Mecânico Redragon','teclado_redragon.jpg','Redragon','Teclado mecânico Redragon Kumara',250.00,0),(5,'Monitor AOC','1750362931081-575058803.jpg','AOC','Monitor AOC     ',350.00,0);
 /*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -174,8 +215,10 @@ CREATE TABLE `usuarios` (
   `nome` varchar(100) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `senha` varchar(255) DEFAULT NULL,
+  `tipo` enum('cliente','administrador') DEFAULT 'cliente',
+  `token_reset` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -184,7 +227,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'Ana Julia Rodrigues','anajulia@gmail.com','anajulia123'),(2,'Guilherme Laurindo','guilherme@gmail.com','guilherme123'),(3,'Miguel Luperi','miguel@gmail.com','miguel123'),(4,'Gabriel Ferrarez','gabriel@gmail.com','gabriel123'),(5,'Eduardo Gilbertoni','eduardo@gmail.com','eduardo123');
+INSERT INTO `usuarios` VALUES (1,'Ana Julia Rodrigues','anajulia@gmail.com','anajulia123','cliente',NULL),(2,'Guilherme Laurindo','guilherme@gmail.com','guilherme123','cliente',NULL),(3,'Miguel Luperi','miguel@gmail.com','miguel123','cliente',NULL),(4,'Gabriel Ferrarez','gabriel@gmail.com','gabriel123','cliente',NULL),(5,'Eduardo Gilbertoni','eduardo@gmail.com','eduardo123','cliente',NULL),(14,'Gabriel Antônio ','gabrielferrarez77@gmail.com','$2b$10$fjt3mc3QjFNAm3CEAU0psueJggGSWhC6zHAe1A1dHOg69BNtf46iK','administrador',NULL),(15,'Gabriel Antônio ','vesgoff777@gmail.com','$2b$10$SwTc2ub5JtR.PoeujGDiduve59snIqVCIEZbEtazrotxFD5RXNQYm','cliente',NULL);
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -197,4 +240,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-15 15:01:16
+-- Dump completed on 2025-06-21 13:14:41
